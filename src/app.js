109ce1d79f0a34c32b6530b4a790a78daee0dc57
Jean Louis DUMAS <dumasjl911@gmail.com>
2023-11-27 16:07:11 +0100

    Quest_01.2

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
@@ -6,5 +8,8 @@ const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", movieControllers.getUsers);
+app.get("/api/users/:id", movieControllers.getUsersById);
 
 module.exports = app;
+
