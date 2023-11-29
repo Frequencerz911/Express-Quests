747718a75e1952b6515da23b5ca51ab55982e74a
Jean Louis DUMAS <dumasjl911@gmail.com>
2023-11-29 11:39:37 +0100

    Quest_2.0_Finish

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -25,6 +25,7 @@ const movies = [
   },
 ];
 
+
 const database = require("../../database");
 
 const getMovies = (req, res) => {
@@ -37,18 +38,6 @@ const getMovies = (req, res) => {
     console.error(err);
     res.sendStatus(500);
   });
-};
-
-const getUsers = (req, res) => {
-  database
-    .query("select * from users")
-    .then(([users]) => {
-      res.json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
 };
 
 const getMovieById = (req, res) => {
@@ -69,27 +58,25 @@ const getMovieById = (req, res) => {
     });
 };
 
-const getUsersById = (req, res) => {
-  const id = parseInt(req.params.id);
+const postMovie = (req, res) => {
+  const { title, director, year, color, duration } = req.body;
 
   database
-  .query("select * from users where id= ?", [id])
-  .then(([users]) => {
-    if (users[0] != null) {
-      res.json(users[0]);
-    } else {
-      res.sendStatus(404);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+    .query(
+      "INSERT INTO movies(title, director, year, color, duration) VALUES (?, ?, ?, ?, ?)",
+      [title, director, year, color, duration]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 module.exports = {
   getMovies,
   getMovieById,
-  getUsers,
-  getUsersById,
+  postMovie,
 };
---
+++ src/controllers/userControllers.js
@@ -0,0 +1,81 @@
+const users = [
+    {
+      id: 1,
+      firstname: "John",
+      lastname: "Doe",
+      email: "john.doe@example.com",
+      city: "Paris",
+      language: "English"
+    },
+    {
+      id: 2,
+      firstname: "Valeriy",
+      lastname: "Appius",
+      email: "valeriy.appius@example.com",
+      city: "Moscow",
+      language: "Russian"
+    },
+    {
+      id: 3,
+      firstname: "Ralf",
+      lastname: "Geronimo",
+      email: "ralf.geronimo@example.com",
+      city: "New York",
+      language: "Italian"
+    },
+  ];
+
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+    database
+      .query("select * from users")
+      .then(([users]) => {
+        res.json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  const getUsersById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+    .query("select * from users where id= ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+  };
+
+  const postUsers = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.status(201).send({ id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  module.exports = {
+    getUsers,
+    getUsersById,
+    postUsers,
+  };
